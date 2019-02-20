4fc5cbf86c412a2e8f2191f0ef32c545c8e55e95
CSanctuary <47758425+CSanctuary@users.noreply.github.com>
2019-02-20 13:02:29 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-document.getElementById('form1').addEventListener('submit', function(evt){
-    evt.preventDefault();
-    document.getElementById('donate').style.display = 'none';
-    document.getElementById('topMessage').style.display = 'none';
-})
+document.getElementById("submit").addEventListener("click", function( event ) {
+  document.getElementById("survey-form").classList.add("submitted");
+});

c312938c5a8fe0a5bb0a1fed38536ac006ca865a
CSanctuary <47758425+CSanctuary@users.noreply.github.com>
2019-02-27 14:45:44 -0600

    Update acknowledge.php

--- acknowledge.php
+++ acknowledge.php
@@ -1,3 +1,14 @@
+<?php
+$headers = "From: googol.msg@gmail.com\r\n"; 
+$headers . = 'Content-Type: text/plain; charset=utf-8'; 
+if (isset($_POST['send'])) {
+$to = 'googol.msg@gmail.com';
+$subject = 'Feedback from my site';
+$message = 'Name: '; . $_POST['name'] . "r\n\r\n";
+$message = 'Email: ' . $_POST['email'] . "r\n\r\n";
+$message = 'Comments: ' . $_POST['comments'];
+}
+?>
 
 <!DOCTYPE html
 PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
